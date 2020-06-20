62623f67b4d1c9fc34c0ff372d181f98c2459f82
AhmadTakkoush <ahmadtakkoush@hotmail.com>
2020-06-20 18:13:23 +0300

    Alpha 1

---
+++ app/src/main/java/com/ahmadtakkoush/appledeveloper/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.ahmadtakkoush.appledeveloper
+
+import android.os.Bundle
+import android.view.KeyEvent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val liveFragment = LiveFragment()
+        val comingsoonFragment = ComingSoonFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_forum -> makeCurrentFragment(homeFragment)
+                R.id.ic_live_tv -> makeCurrentFragment(liveFragment)
+                R.id.ic_help -> makeCurrentFragment(comingsoonFragment)
+
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (WebView.canGoBack()) {
+            WebView.goBack();
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && WebView.canGoBack()) {
+            WebView.goBack()
+            //If there is history, then the canGoBack method will return ‘true’//
+            return true
+        }
+
+        //If the button that’s been pressed wasn’t the ‘Back’ button, or there’s currently no
+        //WebView history, then the system should resort to its default behavior and return
+        //the user to the previous Activity//
+
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper, fragment)
+                commit()
+            }
+
+}
---
+++ app/src/main/java/fragments/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.ahmadtakkoush.appledeveloper
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+
+
+class HomeFragment : Fragment() {
+
+    var mWebView: WebView? = null
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_home, container, false)
+        mWebView = v.findViewById(R.id.WebView) as WebView
+        mWebView!!.loadUrl("https://developer.apple.com/forums")
+
+        // Enable Javascript
+        val webSettings: WebSettings = mWebView!!.settings
+        webSettings.javaScriptEnabled = true
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView!!.webViewClient = WebViewClient()
+        return v
+    }
+}
